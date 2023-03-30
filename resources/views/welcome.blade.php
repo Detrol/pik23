9b9b2d7f42ae5a0d654c2469fa36b9b4326684ec
Detrol <thuno@live.se>
2023-03-30 19:01:43 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,7 @@
     </section>-->
     <section class="relative h-screen">
         <div class="jarallax relative h-screen" data-jarallax data-video-src="mp4:{{ Vite::asset('resources/images/rain.mp4') }}" style="z-index: 1">
-            <div class="absolute bg-gray-900 w-full h-screen opacity-60" style="z-index: 2;"></div>
+            <div class="absolute bg-black w-full h-screen opacity-40" style="z-index: 2;"></div>
             <div class="absolute container mx-auto items-center justify-center text-center py-52" style="z-index: 3">
                 <p class="text-4xl lg:text-6xl font-medium text-white">
                     Välkommen till
