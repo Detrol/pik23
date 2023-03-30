4e5bd718c7e69438eebb2de55e41a193e88ce15f
Detrol <thuno@live.se>
2023-03-30 14:41:58 +0200

    Update base.blade.php

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -19,6 +19,7 @@
     @livewireStyles
 
     <link rel="preload" href="{{ asset('assets/css/thin.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
+    <link rel="preload" href="{{ asset('assets/css/duotone.css') }}" as="style" onload="this.onload=null;this.rel='stylesheet'">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
