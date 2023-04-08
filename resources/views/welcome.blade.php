0403adf1abf428e5b4a5d7fe6a91bf3ae5b5d7c8
Detrol <thuno@live.se>
2023-04-08 12:58:17 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,32 +73,32 @@
                                    data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200">
                                     Hej där!
                                 </p>
-                                <p class="mt-4 block font-sans lg:text-lg text-base leading-relaxed antialiased text-gray-600"
+                                <h1 class="mt-4 block font-sans lg:text-lg text-base leading-relaxed antialiased text-gray-600"
                                    data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200">
                                     Letar du efter en prisvärd och pålitlig fönsterputsare i Karlstad, Hammarö,
                                     Vålberg, Grums, Molkom, Kil eller Väse med omnejd?
-                                </p>
+                                </h1>
                                 <p class="mt-4 block font-sans lg:text-lg font-base leading-relaxed antialiased text-gray-600"
                                    data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200">
                                     Då har du kommit till rätt ställe!
                                 </p>
-                                <p class="mt-4 block font-sans lg:text-lg font-base leading-relaxed antialiased text-gray-600"
+                                <h2 class="mt-4 block font-sans lg:text-lg font-base leading-relaxed antialiased text-gray-600"
                                    data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200">
                                     Jag heter Andreas och jag har över 10 års erfarenhet av fönsterputsning, både för
                                     privatpersoner och företag.
-                                </p>
-                                <p class="mt-4 block font-sans lg:text-lg font-base leading-relaxed antialiased text-gray-600"
+                                </h2>
+                                <h3 class="mt-4 block font-sans lg:text-lg font-base leading-relaxed antialiased text-gray-600"
                                    data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200">
                                     Jag erbjuder professionell hjälp med rätt utrustning och teknik för
                                     att se till att dina fönster blir skinande rena. Oavsett om du har en stor byggnad
                                     eller en liten lägenhet, så har jag kunskapen och verktygen för att klara av
                                     uppdraget.
-                                </p>
-                                <p class="mt-4 block font-sans lg:text-lg font-normal leading-relaxed antialiased text-gray-600"
+                                </h3>
+                                <h3 class="mt-4 block font-sans lg:text-lg font-normal leading-relaxed antialiased text-gray-600"
                                    data-aos="fade-up" data-aos-duration="1000" data-aos-offset="200">
                                     Så varför inte låta mig ta hand om dina fönster? Boka tid hos mig och upplev
                                     skillnaden av kristallklara fönster.
-                                </p>
+                                </h3>
 
 
                                 <!--<div id="textWrapper1" class="textWrapper">
